b51aac2c10896e9e5aaacdbd9857f63189d13837
nawrotech <nawrot13@protonmail.com>
2024-10-11 16:04:12 +0200

    add seat reservation entity

---
+++ src/Entity/ReservationSeat.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationSeatRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+
+#[ORM\Entity(repositoryClass: ReservationSeatRepository::class)]
+class ReservationSeat
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservationSeats')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Showtime $showtime = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?ScreeningRoomSeat $seat = null;
+
+    #[ORM\Column(length: 15)]
+    private ?string $status = null;
+
+    // TO ADD reservation
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getShowtime(): ?Showtime
+    {
+        return $this->showtime;
+    }
+
+    public function setShowtime(?Showtime $showtime): static
+    {
+        $this->showtime = $showtime;
+
+        return $this;
+    }
+
+    public function getSeat(): ?ScreeningRoomSeat
+    {
+        return $this->seat;
+    }
+
+    public function setSeat(?ScreeningRoomSeat $seat): static
+    {
+        $this->seat = $seat;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
---
+++ src/Repository/ReservationSeatRepository.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Repository;
+
+use App\Contracts\SeatsGridInterface;
+use App\Entity\ReservationSeat;
+use App\Entity\ScreeningRoom;
+use App\Entity\Showtime;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<ReservationSeat>
+ */
+class ReservationSeatRepository extends ServiceEntityRepository implements SeatsGridInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ReservationSeat::class);
+    }
+
+    /**
+     * @return int[]
+     */
+    public function findRows(ScreeningRoom $screeningRoom): array
+    {
+        $result = $this->createQueryBuilder('rs')
+            ->select("DISTINCT s.rowNum")
+            ->innerJoin("rs.showtime", "sh")
+            ->innerJoin("sh.screeningRoom", "sr")
+            ->innerJoin("sr.screeningRoomSeats", "srs")
+            ->innerJoin("srs.seat", "cs")
+            ->innerJoin("cs.seat", "s")
+            ->andWhere("sr = :screeningRoom")
+            ->setParameter("screeningRoom", $screeningRoom)
+            ->addOrderBy("s.rowNum", "ASC")
+            ->getQuery()
+            ->getResult();
+     
+        return array_map("intval", array_column($result, "rowNum"));
+    }
+
+
+    /**
+     * @return ReservationSeat[]
+     */
+    public function findSeatsInRow(
+        ScreeningRoom $screeningRoom, 
+        int $rowNum, 
+        ?Showtime $showtime = null): array
+    {
+        return $this->createQueryBuilder('rs')
+            ->innerJoin("rs.showtime", "sh")
+            ->innerJoin("sh.screeningRoom", "sr")
+            ->innerJoin("rs.seat", "srs")
+            ->innerJoin("srs.seat", "cs")
+            ->innerJoin("cs.seat", "s")
+            ->andWhere("rs.showtime = :showtime")
+            ->andWhere("sr = :screeningRoom")
+            ->andWhere("s.rowNum = :rowNum")
+            ->addOrderBy("s.rowNum", "ASC")
+            ->addOrderBy("s.colNum", "ASC")
+            ->setParameter("rowNum", $rowNum)
+            ->setParameter("screeningRoom", $screeningRoom)
+            ->setParameter("showtime", $showtime)
+            ->getQuery()
+            ->getResult()
+            ;
+        
+    }
+
+
+    
+
+}
