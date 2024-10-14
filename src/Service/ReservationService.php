141a2b943f8102ecef3d3dc284d74c363732a69c
nawrotech <nawrot13@protonmail.com>
2024-10-14 16:46:46 +0200

    add functionality for creating reservation

--- src/Service/ReservationService.php
+++ src/Service/ReservationService.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Entity\Reservation;
 use App\Repository\ReservationSeatRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -35,7 +36,34 @@ class ReservationService {
                 throw $e;
         }
         
-        
-       
+    }
+
+    public function createReservation(SessionInterface $session) {
+        $this->em->beginTransaction();
+        try {
+            foreach($session->get("cart") as $seatId) {
+                $reservationSeat = $this->reservationSeatRepository->find($seatId);
+                
+                $reservation = new Reservation();
+                $reservation->setEmail($reservationSeat->getEmail());
+                $reservation->setShowtime($reservationSeat->getShowtime());
+                $reservationSeat->setReservation($reservation);
+    
+                $reservationSeat->setStatus("reserved");
+                $reservationSeat->setStatusLockedExpiresAt(null);
+    
+                $this->em->persist($reservation);
+            }
+
+            $this->em->flush();
+            $this->em->commit();
+
+        } catch (\Exception $e) {
+            $this->em->rollback();
+            throw $e;
+        }
+ 
+        $this->em->flush();
+        $session->clear("cart");
     }
 }
