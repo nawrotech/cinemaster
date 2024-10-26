6d915a7abfd86b96a0346fa0a61f4cf79d1a82c6
nawrotech <nawrot13@protonmail.com>
2024-10-26 17:55:29 +0200

    add remove button for dynamic collection type's elements

---
+++ src/Form/Type/RemoveButtonType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RemoveButtonType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            "attr" => [
+                "data-form-collection-target" => "removeButton",
+                "data-action" => "click->form-collection#removeElement",
+                "class" => "btn btn-danger"
+            ],
+            
+        ]);
+    }
+
+    public function getParent(): string
+    {
+        return ButtonType::class;
+    }
+}
