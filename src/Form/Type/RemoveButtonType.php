19a109325f4a2084d5f31a92ac3fa698407fef39
nawrotech <nawrot13@protonmail.com>
2024-10-26 18:37:37 +0200

    fix RemoveButtonType so is no longer mapped

--- src/Form/Type/RemoveButtonType.php
+++ src/Form/Type/RemoveButtonType.php
@@ -4,10 +4,17 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RemoveButtonType extends AbstractType
 {
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->setMapped(false);
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
