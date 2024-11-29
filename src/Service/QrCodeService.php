acf50963618147e1b656321760ffcfccd1d6e260
nawrotech <nawrot13@protonmail.com>
2024-11-29 12:18:01 +0100

    add qrCode service with method for generating qr codes for reservation

---
+++ src/Service/QrCodeService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Reservation;
+use Endroid\QrCode\Builder\BuilderInterface;
+use Symfony\Component\HttpFoundation\UriSigner;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class QrCodeService {
+
+    public function __construct(
+        private BuilderInterface $qrBuilder,
+        private UrlGeneratorInterface $urlGenerator,
+        private UriSigner $uriSigner,
+        ) {
+    }
+
+    public function generateReservationQrCode(Reservation $reservation): string {
+        $cinemaSlug = $reservation->getShowtime()->getCinema()->getSlug();
+        $reservationId = $reservation->getId();
+
+        $reservationValidationUrl = $this->urlGenerator->generate("app_reservation_validate", [
+            "slug" => $cinemaSlug,
+            "id" => $reservationId
+        ], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $signedUrl = $this->uriSigner->sign($reservationValidationUrl);
+
+        $qrCode = $this->qrBuilder->build(
+            data: $signedUrl,
+        )->getDataUri();
+
+        return $qrCode;
+    
+    }
+}
