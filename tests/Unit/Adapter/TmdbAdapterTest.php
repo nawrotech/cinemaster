e3bdbc2785a1d041fe6dd48d4498a557d76adc6b
nawrotech <nawrot13@protonmail.com>
2025-01-16 19:24:33 +0100

    add tests for calculating and returning records by the TmdbAdapter

--- tests/Unit/Adapter/TmdbAdapterTest.php
+++ tests/Unit/Adapter/TmdbAdapterTest.php
@@ -9,13 +9,12 @@ use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class TmdbAdapterTest extends TestCase
 {
-    public function testGetNbResultsReturnsCorrectTotal()
+    public function testGetNbResultsReturnsCorrectTotal(): void
     {
         $httpClient = $this->createMock(HttpClientInterface::class);
         $response = $this->createMock(ResponseInterface::class);
 
         $httpClient->method('request')
-            ->with('GET', 'https://example.com/api', $this->anything())
             ->willReturn($response);
 
         $response->method('toArray')->willReturn([
@@ -28,7 +27,69 @@ class TmdbAdapterTest extends TestCase
         $totalResults = $adapter->getNbResults();
 
         $this->assertEquals(1000, $totalResults);
+    }
+
+    public function testGetNbResultsHandles500PageLimit(): void 
+    {
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+
+        $httpClient->method('request')
+            ->willReturn($response);
         
+        $response->method('toArray')->willReturn(['total_results' => 20000]);
+
+        assert($httpClient instanceof HttpClientInterface);
+
+        $adapter = new TmdbAdapter($httpClient, 'https://example.com', [], 'api_key');
+        $this->assertEquals(10000, $adapter->getNbResults());
+
+    }
+
+    public function testGetSliceRetursResult(): void {
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+
+       
+        $httpClient
+            ->method("request")
+            ->willReturn($response);
+
+        $response->method('toArray')->willReturn([
+            'results' => [
+                ["id" => 1, "title" => "Movie 1"],
+                ["id" => 2, "title" => "Movie 2"]
+            ]
+        ]);
+
+        assert($httpClient instanceof HttpClientInterface);
+        $adapter = new TmdbAdapter($httpClient, 'https://example.com/api', [], 'api_key');
+
+        $results = $adapter->getSlice(20, 20);
+
+        $this->assertCount(2, $results);
+        $this->assertEquals('Movie 1', $results[0]['title']);
+    }
+
+
+    public function testGetSliceHandlesEmptyResults()
+    {
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+
+        $httpClient->method('request')
+            ->willReturn($response);
+
+        $response->method('toArray')->willReturn([
+            'results' => [],
+        ]);
+
+        assert($httpClient instanceof HttpClientInterface);
+
+        $adapter = new TmdbAdapter($httpClient, 'https://example.com', [], 'api_key');
+        $results = $adapter->getSlice(20, 20);
+
+        $this->assertEmpty($results);
     }
 
 }
